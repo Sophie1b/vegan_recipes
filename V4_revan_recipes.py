47113b5f0db63c16a5a5ec643ca5abffcd5d72c7
Sophie1b <35220003+Sophie1b@users.noreply.github.com>
2021-12-15 15:28:41 +0100

    Update V4_revan_recipes.py

--- V4_revan_recipes.py
+++ V4_revan_recipes.py
@@ -197,6 +197,6 @@ sparse_matrix = tfidf.fit_transform(top_50['ingredients_clean_processed'])
 recipe_to_copy_nb = st.slider(" ", min_value=0, max_value=6, step=1)
 recipe_to_copy_nb
 
-recommended = recommended_recipes(top_50,recipe_to_copy_nb,sparse_matrix,5, metric='cosine')
+recommended = recommended_recipes(top_50,recipe_to_copy_nb,sparse_matrix,6, metric='cosine')
 
 recommended
